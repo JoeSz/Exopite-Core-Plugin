ba1a383b4ba426797f47c366cdbabdf3ff72a450
Joe <ujbeszel@gmail.com>
2017-06-12 11:38:08 +0200

    Update version

--- exopite-core.php
+++ exopite-core.php
@@ -5,7 +5,7 @@
  * it is not enqueued multiple times.
  *
  * @link              http://joe.szalai.org
- * @since             1.0.0
+ * @since             1.0.1
  * @package           Exopite_Core
  *
  * @wordpress-plugin
