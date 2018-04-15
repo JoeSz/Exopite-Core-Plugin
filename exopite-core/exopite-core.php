73b4f3df02504be0ea63b74826337c74107d5e4b
Joe <ujbeszel@gmail.com>
2018-04-15 09:24:05 +0200

    Update Bootstrap 4.1.0 and version

--- exopite-core/exopite-core.php
+++ exopite-core/exopite-core.php
@@ -5,7 +5,7 @@
  * it is not enqueued multiple times.
  *
  * @link              http://joe.szalai.org
- * @since             20171202
+ * @since             20180415
  * @package           Exopite_Core
  *
  * @wordpress-plugin
@@ -87,14 +87,9 @@ if ( ! function_exists( 'load_exopite_core_scripts' ) ) {
 if ( ! function_exists( 'load_exopite_core_scripts_frontend_only' ) ) {
     function load_exopite_core_scripts_frontend_only() {
 
-        if ( ! wp_script_is( 'jquery-popper-1123' ) ) {
-            wp_enqueue_script( 'jquery-popper-1123', 'http' . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . '://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', array( 'jquery' ), '1.12.3', true );
-        }
+        wp_enqueue_script( 'jquery-popper-1140', 'http' . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . '://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js', array( 'jquery' ), '1.14.0', true );
 
-        if ( ! wp_script_is( 'bootstrap-4-js' ) ) {
-            wp_register_script( 'bootstrap-4-js', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js", array( 'jquery', 'jquery-popper-1110' ), '4.0.0-beta.2', true );
-            wp_enqueue_script( 'bootstrap-4-js' );
-        }
+        wp_enqueue_script( 'bootstrap-41-js', 'http' . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js", array( 'jquery', 'jquery-popper-1140' ), '4.1.0', true );
 
     }
 }
@@ -107,10 +102,7 @@ if ( ! function_exists( 'load_exopite_core_styles_frontend_only' ) ) {
          *
          * Get Bootstrap 4
          */
-        if ( ! wp_style_is( 'bootstrap-4' ) ) {
-            wp_register_style( 'bootstrap-4', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", false, '4.0.0-beta.2' );
-            wp_enqueue_style( 'bootstrap-4' );
-        }
+        wp_enqueue_style( 'bootstrap-41', 'http' . ($_SERVER['SERVER_PORT'] == 443 ? 's' : '' ) . '://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css', false, '4.1.0' );
 
     }
 }
@@ -118,11 +110,7 @@ if ( ! function_exists( 'load_exopite_core_styles_frontend_only' ) ) {
 if ( ! function_exists( 'load_exopite_core_styles' ) ) {
     function load_exopite_core_styles() {
 
-        if ( ! wp_style_is( 'font-awesome-470' ) ) {
-            /* Get font awsome */
-            wp_register_style( 'font-awesome-470', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css", false, '470' );
-            wp_enqueue_style( 'font-awesome-470' );
-        }
+        wp_enqueue_style( 'font-awesome-470', 'http' . ($_SERVER['SERVER_PORT'] == 443 ? 's': '' ) . '://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', false, '470' );
 
         load_exopite_core_scripts();
 
